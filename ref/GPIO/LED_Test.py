0fe1dfa02fe2761b24a974da227cb4231611b46a
Sam <Sam_Kuo@compal.com>
2016-08-18 13:54:48 +0800

    [GPIO] add ref/LED_Test.py

---
+++ ref/GPIO/LED_Test.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(21, GPIO.OUT)
+
+while True:
+  GPIO.output(21, 0)
+  time.sleep(0.5)
+  GPIO.output(21, 1)
+  time.sleep(0.5)
